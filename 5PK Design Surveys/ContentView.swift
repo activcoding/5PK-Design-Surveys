3b49d0d0250e51dc88dde77f2d7abbedddb8f85d
Tom Ludwig <83090745+activcoding@users.noreply.github.com>
2023-09-12 11:28:11 +0200

    Initial Commit

---
+++ 5PK Design Surveys/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  5PK Design Surveys
+//
+//  Created by Tommy Ludwig on 12.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 5PK Design Surveys/_PK_Design_SurveysApp.swift
@@ -0,0 +1,17 @@
+//
+//  _PK_Design_SurveysApp.swift
+//  5PK Design Surveys
+//
+//  Created by Tommy Ludwig on 12.09.23.
+//
+
+import SwiftUI
+
+@main
+struct _PK_Design_SurveysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
